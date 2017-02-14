814566ead73658a13c348a3b3dfa782b8c1962f7
Ben Holland <benholland@Bens-MacBook-Pro.local>
2017-02-14 01:54:53 +0000

    got component working with v-for and v-bind shows data from an object

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
+Vue.component('origami-piece', {
+  // The todo-item component now accepts a
+  // "prop", which is like a custom attribute.
+  // This prop is called todo.
+  props: ['origami'],
+  template: '<div class="container"><h1> {{ origami.name }} </h1> <br> <img :src="origami.link" alt="" ></div>'
+})
 var app = new Vue({
-  el: '#app',
+  el: '.content',
   data: {
-    message: 'Hello Vue!'
+      origami: [
+        {name: 'Inside Reverse Fold', link: 'http://www.origami-resource-center.com/images/101InsideReverse2.jpg'},
+        {name: 'Outside Reverse Fold', link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Orisymbol_outside_reverse_fold.svg/500px-Orisymbol_outside_reverse_fold.svg.png'}        
+      ],
+  },
+  
+  methods: {
   }
 })
