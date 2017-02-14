ef1d51da139f911a66762f9c31173fa8315a24b4
Ben Holland <benholland@Bens-MacBook-Pro.local>
2017-02-14 01:34:20 +0000

    added randint functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 var app = new Vue({
-  el: '#app',
+  el: '.content',
   data: {
-    message: 'Hello Vue!'
+      array: [],
+      min: 0,
+      max: 0,
+  },
+  
+  methods: {
+    addRandomInt: function() {
+        this.array.push(Math.floor((Math.random() * this.max) + this.min));
+    }
   }
 })
